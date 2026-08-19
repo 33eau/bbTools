29e87c8e37d4fc7b5722b81cfc759f2bde3241b3
Beau.A <bapissara@gmail.com>
2026-08-19 15:54:08 -0700

    Create rig_config.py

---
+++ core/data/rig_config.py
@@ -0,0 +1,15 @@
+ik_base_ctrl_shape = 'box'
+ik_ctrl_shape = 'box'
+fk_ctrl_shape = 'cross_circle'
+switch_ctrl_shape = 'square_round_cross'
+pv_ctrl_shape = 'diamond'
+hock_ctrl_shape = 'heptagon'
+foot_ctrl_shape = 'ball_foot'
+
+ik_leg_shape = 'boxGround'
+
+ribbon_ctrl_shape = 'square_round'
+tweaker_ctrl_shape = 'sphere'
+
+setting_color = 'orange'
+
